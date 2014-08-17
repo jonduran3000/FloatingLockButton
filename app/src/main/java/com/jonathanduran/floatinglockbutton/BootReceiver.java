eb7f3913d1afe7d3ba00f33efce5de37a66372ff
jonduran3000 <jonduran3000@gmail.com>
2014-08-17 19:13:05 -0400

    Restarts the service on device boot.

---
+++ app/src/main/java/com/jonathanduran/floatinglockbutton/BootReceiver.java
@@ -0,0 +1,25 @@
+package com.jonathanduran.floatinglockbutton;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+/**
+ * Created by jonathanduran on 8/17/14.
+ */
+public class BootReceiver extends BroadcastReceiver {
+    private SharedPreferences preferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        preferences = context.getSharedPreferences(MainActivity.PREF, Context.MODE_PRIVATE);
+
+        boolean serviceEnabled = preferences.getBoolean(MainActivity.BUTTON_DISPLAYED, false);
+
+        if(serviceEnabled) {
+            Intent floatingService = new Intent(context, FloatingButtonService.class);
+            context.startService(floatingService);
+        }
+    }
+}
